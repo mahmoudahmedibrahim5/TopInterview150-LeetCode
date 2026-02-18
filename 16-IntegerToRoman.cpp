33e21fd7501625a77c9c9263b9972c44ee275860
Mahmoud Ahmed <mahmoudahmedibrahim5@gmail.com>
2026-02-19 00:31:31 +0200

    Accepted

---
+++ 16-IntegerToRoman.cpp
@@ -0,0 +1,84 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string result;
+        while(num >= 1000)
+        {
+            result.push_back('M');
+            num -= 1000;
+        }
+        if(num >= 900)
+        {
+            result.push_back('C');
+            result.push_back('M');
+            num -= 900;
+        }
+
+        while(num >= 500)
+        {
+            result.push_back('D');
+            num -= 500;
+        }
+        if(num >= 400)
+        {
+            result.push_back('C');
+            result.push_back('D');
+            num -= 400;
+        }
+
+        while(num >= 100)
+        {
+            result.push_back('C');
+            num -= 100;
+        }
+        if(num >= 90)
+        {
+            result.push_back('X');
+            result.push_back('C');
+            num -= 90;
+        }
+
+        while(num >= 50)
+        {
+            result.push_back('L');
+            num -= 50;
+        }
+        if(num >= 40)
+        {
+            result.push_back('X');
+            result.push_back('L');
+            num -= 40;
+        }
+
+        while(num >= 10)
+        {
+            result.push_back('X');
+            num -= 10;
+        }
+        if(num >= 9)
+        {
+            result.push_back('I');
+            result.push_back('X');
+            num -= 9;
+        }
+
+        while(num >= 5)
+        {
+            result.push_back('V');
+            num -= 5;
+        }
+        if(num >= 4)
+        {
+            result.push_back('I');
+            result.push_back('V');
+            num -= 4;
+        }
+
+        while(num >= 1)
+        {
+            result.push_back('I');
+            num -= 1;
+        }
+        return result;
+    }
+};
